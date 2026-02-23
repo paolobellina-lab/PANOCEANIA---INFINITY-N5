0c6a79d208fe3f9673cc3f9117c329e1622ee66f
paolobellina-lab <paolo.bellina@telmotor.it>
2026-02-23 21:47:02 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-self.addEventListener('fetch', () => {});
+// Questo file è obbligatorio per l'installazione della PWA
+const CACHE_NAME = 'pano-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Lasciamo il fetch vuoto per bypassare la cache offline 
+  // ma soddisfare i requisiti di installazione di Android
+  event.respondWith(fetch(event.request));
+});
